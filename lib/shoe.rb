3f53e9106b4d9ba49ad16afcc812148ac74c90a3
Princeton Rose <decoyzpr@gmail.com>
2019-03-13 17:53:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+class Shoe
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def cobble
+    puts "The shoe has been repaired."
+    @condition = "new"
+  end
+
+
+
+end
